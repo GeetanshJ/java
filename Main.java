cd9ea7ce4138b0eda018954eeff11e863fd4056a
Geetansh Jain <jaingeetansh@gmail.com>
2025-06-23 10:24:15 +0530

    .

--- Main.java
+++ Main.java
@@ -1,55 +1,66 @@
+// class A{
+//     static  class C{
+//         public void hello(){
+//             System.out.println("Hello from C");
+//         }
+//     }
+
+//     public A(){
+//         System.out.println("Constructor A");
+//     }
+
+//     public void show(){
+//         System.out.println("A show");
+//     }
+// }
+
+// class B extends A{
+//     public B(){
+//         System.out.println("Constructor B");
+//     }
+
+//     @Override
+//     public void show(){
+//         System.out.println("B show");
+//     }
+// }
+
+// class D extends B{
+//     public D(){
+//         System.out.println("Constructor D");
+//     }
+
+//     @Override
+//     public void show(){
+//         System.out.println("D show");
+//     }
+
+//     protected  static void dd(){
+//         System.out.println("DD");
+//     }
+// }
 class A{
-    static  class C{
-        public void hello(){
-            System.out.println("Hello from C");
-        }
-    }
+    int a;
 
-    public A(){
-        System.out.println("Constructor A");
-    }
-
-    public void show(){
-        System.out.println("A show");
-    }
-}
-
-class B extends A{
-    public B(){
-        System.out.println("Constructor B");
-    }
-
-    @Override
-    public void show(){
-        System.out.println("B show");
-    }
-}
-
-class C extends B{
-    public C(){
-        System.out.println("Constructor C");
-    }
-
-    @Override
-    public void show(){
-        System.out.println("C show");
-    }
-
-    protected  static void dd(){
-        System.out.println("DD");
+    public void abc(){
+        int b;
+        System.out.println(b);
+        System.out.println(a);
     }
 }
 
 public class Main{
+
     public Main(){
+    
         System.out.println("Inside Main constructor");
     }
 
     public static void main(String[] args) {    
-        A obj = new A();
-        // A.C obj1 = obj.new C();
+        // A obj = new A();
+        // // A.C obj1 = obj.new C();
 
-        A.C obj2 = new A.C();
-        obj2.hello();
+        // A.C obj2 = new A.C();
+        // obj2.hello();
     }
 }  
