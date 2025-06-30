9a86a5d241ff80650c188a52332b6b1b93ab07a8
GeetanshJ <jaingeetansh@gmail.com>
2025-06-30 14:42:32 +0530

    .

--- Main.java
+++ Main.java
@@ -1,66 +1,12 @@
-// class A{
-//     static  class C{
-//         public void hello(){
-//             System.out.println("Hello from C");
-//         }
-//     }
-
-//     public A(){
-//         System.out.println("Constructor A");
-//     }
-
-//     public void show(){
-//         System.out.println("A show");
-//     }
-// }
-
-// class B extends A{
-//     public B(){
-//         System.out.println("Constructor B");
-//     }
-
-//     @Override
-//     public void show(){
-//         System.out.println("B show");
-//     }
-// }
-
-// class D extends B{
-//     public D(){
-//         System.out.println("Constructor D");
-//     }
-
-//     @Override
-//     public void show(){
-//         System.out.println("D show");
-//     }
-
-//     protected  static void dd(){
-//         System.out.println("DD");
-//     }
-// }
-class A{
-    int a;
-
-    public void abc(){
-        int b;
-        System.out.println(b);
-        System.out.println(a);
+interface  A{
+    public static void bad(){
+ 
     }
 }
 
+
 public class Main{
-
-    public Main(){
-    
-        System.out.println("Inside Main constructor");
+    public static void main(String[] args) {
+        
     }
-
-    public static void main(String[] args) {    
-        // A obj = new A();
-        // // A.C obj1 = obj.new C();
-
-        // A.C obj2 = new A.C();
-        // obj2.hello();
-    }
-}  
+}
