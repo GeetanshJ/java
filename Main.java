efd6b65cb6ae55073af91927167e180ff55cc6fc
GeetanshJ <jaingeetansh@gmail.com>
2025-07-16 21:01:44 +0530

    ;

--- Main.java
+++ Main.java
@@ -6,7 +6,20 @@ interface  A{
 
 
 public class Main{
+
+    int a = 4; 
+
     public static void main(String[] args) {
-        
+        int a = 4; 
+        System.out.println(a);
+        {
+            System.out.println(a);
+            a = 4;
+            System.out.println(a);
+        }
+
+
+
+        System.out.println(a);
     }
 }
